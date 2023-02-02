5ba11aa1c1052d2652abea04be59204914e87b7f
肖笛根 <xiaodigen@yzgong.com>
2023-02-02 17:15:26 +0800

    ci: 推送脚步

--- deploy.sh
+++ deploy.sh
@@ -12,14 +12,17 @@ npm run build
 
 cd docs/.vuepress/dist
 
+# 如果是发布到自定义域名
+# echo 'www.example.com' > CNAME
+
 git init
-
 git add -A
-
 git commit -m 'deploy'
 
-# 如果发布到 https://<USERNAME>.github.io/<REPO>
+# 如果发布到 https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
+# 如果发布到 https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:Dickens-Shaw/documents.git master:gh-pages
 
 cd -
