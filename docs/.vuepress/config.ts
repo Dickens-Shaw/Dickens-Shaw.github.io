d57a89bd3076965e7a8fe975387ee978b3a6e902
肖笛根 <xiaodigen@yzgong.com>
2023-08-08 14:32:19 +0800

    feat: key

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -3,14 +3,13 @@ import { defaultTheme } from '@vuepress/theme-default';
 import { navbar} from './config/navbar';
 import { sidebar } from './config/sidebar';
 import { docsearchPlugin } from '@vuepress/plugin-docsearch'
-
 export default defineUserConfig({
   lang: 'zh-CN',
   title: 'Shaw',
   description: '这是一个 VuePress 站点',
   base: '/',
   plugins: [
-    docsearchPlugin({
+    docsearchPlugin({ // key: 35493897496dea045e0c647a857e6f5b
       appId: 'UMHCL6J1WP',
       apiKey: '8d3c98c15e1d2ea1cc1f841fddeabeea',
       indexName: 'dickens-shawio',
