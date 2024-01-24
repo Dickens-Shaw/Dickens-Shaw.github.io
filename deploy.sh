cf3cd526b5586dca58bd90a7bce36bf571bed714
肖笛根 <xiaodigen@yzgong.com>
2024-01-24 10:42:54 +0800

    feat: deploy6

--- deploy.sh
+++ deploy.sh
@@ -1,28 +1,28 @@
-#!/usr/bin/env sh
+# #!/usr/bin/env sh
 
-# 确保脚本抛出遇到的错误
+# # 确保脚本抛出遇到的错误
 
-set -e
+# set -e
 
-# 生成静态文件
+# # 生成静态文件
 
-npm run build
+# npm run build
 
-# 进入生成的文件夹
+# # 进入生成的文件夹
 
-cd dist
+# cd dist
 
-# 如果是发布到自定义域名
-# echo 'www.example.com' > CNAME
+# # 如果是发布到自定义域名
+# # echo 'www.example.com' > CNAME
 
-git init
-git add -A
-git commit -m 'deploy'
+# git init
+# git add -A
+# git commit -m 'deploy'
 
-# 如果发布到 https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
+# # 如果发布到 https://<USERNAME>.github.io
+# # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
-# 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:Dickens-Shaw/Dickens-Shaw.github.io.git master:gh-pages
+# # 如果发布到 https://<USERNAME>.github.io/<REPO>
+# git push -f git@github.com:Dickens-Shaw/Dickens-Shaw.github.io.git master:gh-pages
 
-cd -
+# cd -
