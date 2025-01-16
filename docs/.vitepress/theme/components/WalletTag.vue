ec1e85ffb17d7175409bd654a82aa739b2d75ecd
Dickens Shaw <1016598922@qq.com>
2025-01-16 13:00:58 +0800

    Update WalletTag.vue

--- docs/.vitepress/theme/components/WalletTag.vue
+++ docs/.vitepress/theme/components/WalletTag.vue
@@ -1,11 +1,16 @@
 <template>
   <div>
     <div v-for=" wallet in walletSupport" :key="wallet.name">
-      {{ wallet.name }} web: {{ wallet.web ? '✔' : '✘' }} mobile: {{ wallet.mobile ? '✔' : '✘' }}
+      <div>[{{ wallet.name }}]: web: {{ wallet.web ? '✔' : '✘' }} mobile: {{ wallet.mobile ? '✔' : '✘' }}</div>
+      <ul>
+        <li v-for="(value, key) in wallet.envs" :key="key">
+          window.{{ key.replace('_', '.') }}: {{ value }}
+        </li>
+      </ul>
     </div>
     <p></p>
-    <div>UserAgent: {{clientInfo.userAgent}}</div>
-    <div>Vendor: {{clientInfo.vendor}}</div>
+    <div>UserAgent: {{ clientInfo.userAgent }}</div>
+    <div>Vendor: {{ clientInfo.vendor }}</div>
   </div>
 </template>
 
@@ -22,55 +27,98 @@ onMounted(() => {
     {
       name: 'ArgentX',
       web: window.starknet || window.starknet_argentX,
-      mobile: window.starknet_argentX // && window.starknet_argentX.isInAppBrowser
+      mobile: window.starknet_argentX,
+      envs: {
+        starknet: window.starknet ? '✔' : '✘',
+        starknet_argentX: window.starknet_argentX ? '✔' : '✘',
+      },
     },
     // BitGet
     {
       name: 'Bitget',
       web: window.bitgetWallet || window.bitgetTonWallet || window.bitkeep || window.ethereum?.isBitKeep || window.ethereum?.isBitKeepChrome,
-      mobile: window.bitgetWallet || window.bitgetTonWallet || window.bitkeep
+      mobile: window.bitgetWallet || window.bitgetTonWallet || window.bitkeep,
+      envs: {
+        bitgetWallet: window.bitgetWallet ? '✔' : '✘',
+        bitgetTonWallet: window.bitgetTonWallet ? '✔' : '✘',
+        bitkeep: window.bitkeep ? '✔' : '✘',
+        ethereum_isBitKeep: window.ethereum?.isBitKeep ? '✔' : '✘',
+        ethereum_isBitKeepChrome: window.ethereum?.isBitKeepChrome ? '✔' : '✘',
+      },
     },
     // CoinBase
     {
       name: 'CoinBase',
       web: window.coinbaseWalletExtension || window.ethereum?.isCoinbaseWallet,
-      mobile: window.coinbaseWallet || window.ethereum?.isCoinbaseBrowser
+      mobile: window.coinbaseWallet || window.ethereum?.isCoinbaseBrowser,
+      envs: {
+        coinbaseWalletExtension: window.coinbaseWalletExtension ? '✔' : '✘',
+        ethereum_isCoinbaseWallet: window.ethereum?.isCoinbaseWallet ? '✔' : '✘',
+        coinbaseWallet: window.coinbaseWallet ? '✔' : '✘',
+        ethereum_isCoinbaseBrowser: window.ethereum?.isCoinbaseBrowser ? '✔' : '✘',
+      },
     },
     // MetaMask
     {
       name: 'MetaMask',
       web: window.ethereum?.isMetaMask,
-      mobile: window.metamask || window.ethereum
+      mobile: window.metamask || window.ethereum,
+      envs: {
+        ethereum_isMetaMask: window.ethereum?.isMetaMask ? '✔' : '✘',
+        metamask: window.metamask ? '✔' : '✘',
+        ethereum: window.ethereum ? '✔' : '✘',
+      },
     },
     // Phantom
     {
       name: 'Phantom',
       web: window.phantom,
-      mobile: window.phantom
+      mobile: window.phantom,
+      envs: {
+        phantom: window.phantom ? '✔' : '✘',
+      },
     },
     // TokenPocket
     {
       name: 'TokenPocket',
       web: window.tokenpocket || window.ethereum?.isTokenPocket,
-      mobile: window.tokenpocket
+      mobile: window.tokenpocket,
+      envs: {
+        tokenpocket: window.tokenpocket ? '✔' : '✘',
+        ethereum_isTokenPocket: window.ethereum?.isTokenPocket ? '✔' : '✘',
+      },
     },
     // TronLink
     {
       name: 'TronLink',
       web: window.tronLink || window.ethereum?.isTronLink,
-      mobile: window.tronLink
+      mobile: window.tronLink,
+      envs: {
+        tronLink: window.tronLink ? '✔' : '✘',
+        ethereum_isTronLink: window.ethereum?.isTronLink ? '✔' : '✘',
+      },
     },
     // TrustWallet
     {
       name: 'TrustWallet',
       web: window.trustwallet || window.trustWallet || window.ethereum?.isTrust || window.ethereum?.isTrustWallet,
-      mobile: window.trustwallet || window.trustWallet
+      mobile: window.trustwallet || window.trustWallet,
+      envs: {
+        trustwallet: window.trustwallet ? '✔' : '✘',
+        trustWallet: window.trustWallet ? '✔' : '✘',
+        ethereum_isTrust: window.ethereum?.isTrust ? '✔' : '✘',
+        ethereum_isTrustWallet: window.ethereum?.isTrustWallet ? '✔' : '✘',
+      },
     },
     // 欧意 OKX
     {
       name: 'OKX',
       web: window.okexchain || window.okxwallet,
-      mobile:  window.okexchain || window.okxwallet
+      mobile: window.okexchain || window.okxwallet,
+      envs: {
+        okexchain: window.okexchain ? '✔' : '✘',
+        okxwallet: window.okxwallet ? '✔' : '✘',
+      },
     },
   ]
 
